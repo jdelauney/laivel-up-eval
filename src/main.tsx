b7c477acff13c1ea7fe91c7279334b69576a983f
jdelauney <delauney.jerome@gmail.com>
2026-08-29 10:13:52 +0200

    refactor: remove SessionProvider and composition from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { composition } from './composition-root'
-import { SessionProvider } from './session-context'
+
 
 const rootElement = document.getElementById('root')
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <SessionProvider composition={composition}>
+
         <App />
-      </SessionProvider>
+
     </StrictMode>,
   )
 }
